aad5d28c1f36b7e931149f19e497d2f9fa1ac4f4
indrakumarmhaski <indrakumarmhaski@gamil.com>
2018-07-09 16:01:32 +0530

    Readded the index action to events and styled index and myevents page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  resources :events, only: [:show, :edit, :update, :new, :destroy]
+  resources :events
   root to: 'pages#home'
   get 'pages/home'
   get 'pages/about'
